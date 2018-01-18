7de0692846c10f4c8346ee65a442c99f40b602f3
Tony Lin <tonylin1208@gmail.com>
2018-01-18 11:46:13 +0800

    add seed of 10 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |user|
+  user = User.create(name: "User_#{user+1}")
+  puts "#{user.name} is been created!"
+end
